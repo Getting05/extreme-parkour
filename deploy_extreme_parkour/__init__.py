9158875933a605c5ac71d1dd64d210037c7f85b5
Getting <155835254+Getting05@users.noreply.github.com>
2026-05-30 15:13:05 +0800

    Add heightmap deployment package

---
+++ deploy_extreme_parkour/__init__.py
@@ -0,0 +1,5 @@
+"""Deployment utilities for the mybot_v3 Extreme Parkour heightmap student.
+
+This package is intentionally independent from Isaac Gym.  It is used by both
+MuJoCo sim2sim and real-robot control loops.
+"""
